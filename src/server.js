04285c7eb977c9515297ee8e3564e22c79d88aab
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2024-08-22 17:14:21 -0300

    feat:creat server

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import {config} from 'dotenv'
+
+config()
+const serverPort = process.env.PORT || 3000
+
+const app =  express()
+app.use(express.json())
+
+app.get("/",(req,res) => {
+return res.status(200).send({message:"Hello, Word!"})
+})
+
+app.listen(serverPort, () => {
+    console.log(`✨ Server started on http://localhost:${serverPort}`)
+})
