35a2f05c4b254bdfbd3e41b0d5115fad5726ea07
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2024-08-29 11:50:57 -0300

    refactor: start to refactor api routes

---
+++ src/routes/personagens.routes.js
@@ -0,0 +1,29 @@
+const personagens = [
+    {
+    id: 100,
+    nome: "Totoro",
+    vivo: true
+    },
+    
+    {
+    id: 101,
+    nome: "Edward Cullen",
+    vivo: false
+    },
+    
+    {
+        id:102,
+        nome: "Jade",
+        vivo: true
+    },
+    
+    {
+        id:103,
+        nome:"Mulan",
+        vivo:true
+    
+    }
+    ]
+
+
+    
--- src/server.js
+++ src/server.js
@@ -7,58 +7,12 @@ const serverPort = process.env.PORT || 3000
 const app =  express()
 app.use(express.json())
 
-const emocoes = [
-    {
-        id:1,
-        nome: "Nojinho",
-        cor: "Verde"
- },
- {
- id:2,
- nome: "Ansiedade",
- cor: "Laranja"
-},
-{
-    id:3,
-    nome: "Medo",
-    cor: "Roxo"
-}
-]
 
-const personagens = [
-{
-id: 100,
-nome: "Totoro",
-vivo: true
-},
-
-{
-id: 101,
-nome: "Edward Cullen",
-vivo: false
-},
-
-{
-    id:102,
-    nome: "Jade",
-    vivo: true
-},
-
-{
-    id:103,
-    nome:"Mulan",
-    vivo:true
-
-}
-]
 
 app.get("/",(req,res) => {
     return res.status(200).send( emocoes)
     })
 
-    app.get("/emocoes",(req,res) => {
-        return res.status(200).send(emocoes)
-        })
 
 
 app.get("/2tds2",(req,res) => {
@@ -69,16 +23,7 @@ app.get("/2tds2",(req,res) => {
         return res.status(200).send( personagens )
         })
 
-        app.post("/emocoes",(req,res) => {
-            const { nome,cor } = req.body
-            const novaEmocao = {
-                id:emocoes.length + 1,
-                nome: nome,
-                cor: cor
-            }
-            emocoes.push(novaEmocao)
-            return res.status(200).send( emocoes )
-            })
+        
 
 app.listen(serverPort, () => {
     console.log(`✨ Server started on http://localhost:${serverPort}`)
