2e357193689be8a9306d4cd816017fc21d153754
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2024-09-26 15:14:20 -0300

    feat: create delete emotion method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -77,5 +77,43 @@ emocoesRoutes.put("/:id",(req,res) => {
 });
 }); 
 
+emocoesRoutes.delete("/:id",(req,res) => {
+    const{id} = req.params;
+    //console.log(id);
+    const emocao = emocoes.find((emotion) =>emotion.id == id )
+    
+    if (!emocao) {
+    return res.status(404).send ({
+    message: "Emoção não enconrada!",
+    });
+    }
+    return res.status(200).send({
+    message:"Emoção encontrada",
+    emocao,
+    });
+    
+});
+    
+    emocoesRoutes.delete("/:id",(req,res) => {
+        const {id} = req.params;
+    
+        const emocao = emocoes.filter((emotion) =>emotion.id == id );
+    
+        if (!emocao) {
+            return res.status(404).send ({
+            message: "Emoção não encontrada!",
+            });
+            }
+        
+            emocoes = emocoes.filter((emotion) => emotion.id != id)
+
+            return res.status(200).send({
+                message:"Emoção deletada",
+                emocao,
+                });
+
+            });
+
+
 
         export default emocoesRoutes;
